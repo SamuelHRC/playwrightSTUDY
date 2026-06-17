faec55699826024b4674bba6e7af487f097bd067
SamuelHRC <samuelhr.coelho@gmail.com>
2026-06-17 14:15:02 -0300

    uso do contains

--- tests/xpathlocators.spec.ts
+++ tests/xpathlocators.spec.ts
@@ -12,4 +12,12 @@ test("Xpath demo in playwright", async ({page}) => {
     //2. Relative xpath
    const relativelogo: Locator = page.locator("//img[@alt='Tricentis Demo Web Shop']")
     await expect(relativelogo).toBeVisible();
+
+    //3. contains(): o caminho do exemplo contem mais de um elemento.
+    /*
+    a) O nosso expect() aqui está apenas comparando a quantidade, logo, não é necessário usar o await, pois não estamos validando a visibilidade ou algo do tipo, apenas a quantidade de elementos que existem com esse caminho.
+    */
+    const products: Locator = page.locator("//h2/a[contains(@href,'computer')]")
+    const productsCount: number = await products.count() // conta quantos elementos existem com esse caminho
+    expect(productsCount).toBeGreaterThan(0) // valida se a quantidade de elementos é maior que 0 |Outros: igual a, menor que, etc.
 })
